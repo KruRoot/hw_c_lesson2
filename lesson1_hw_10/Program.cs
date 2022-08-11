0e7198c340deb1b87afde3eb02b9ba58b8d23151
Andrei <andreyyuryev@yahoo.com>
2022-08-11 22:59:01 +0400

    add lesson1_hw_10

---
+++ lesson1_hw_10/Program.cs
@@ -0,0 +1,11 @@
+﻿// Homework for lesson 2, #10
+Console.WriteLine("Enter the number");
+int num = int.Parse(Console.ReadLine()); int first_num = num / 100; int for_get_second_num = first_num * 100; int second_num = (num - for_get_second_num) / 10;
+if (num > 99 && num < 1000)
+    {
+    Console.WriteLine(second_num);
+    }
+else
+    {
+    Console.WriteLine("This number is not three-digit number");
+    }
